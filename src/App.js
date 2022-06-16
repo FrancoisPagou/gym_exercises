33f00ad770f710125a1aa6d57cd2a7a1c80fa12b
FrancoisPagou <larrylalong21@gmail.com>
2022-06-16 22:27:19 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Box from '@mui/material'
+
+import './App.css'
+
+function App() {
+  return (
+    <Box width="400px">
+        NavBar
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+        </Routes>
+    </Box>
+  )
+}
+
+export default App
